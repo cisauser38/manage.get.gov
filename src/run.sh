6a925d358ab1b69a261830f9deb1f4f1fe8f4890
David Kennedy <david.kennedy@associates.cisa.dhs.gov>
2024-02-06 16:17:35 -0500

    running gunicorn in docker; running statsd in docker (not quite working yet)

--- src/run.sh
+++ src/run.sh
@@ -6,4 +6,14 @@ set -o pipefail
 # Make sure that django's `collectstatic` has been run locally before pushing up to any environment,
 # so that the styles and static assets to show up correctly on any environment.
 
-gunicorn --worker-class=gevent --worker-connections=1000 --workers=1 registrar.config.wsgi -t 60
+# Default command
+cmd="gunicorn --worker-class=gevent registrar.config.wsgi -t 60"
+
+# Check if --bind argument is provided
+if [[ "$#" -eq 1 && "$1" == "--bind"* ]]; then
+    # Append the provided bind argument to the command
+    cmd="$cmd $1"
+fi
+
+# Execute the command
+eval "$cmd"
